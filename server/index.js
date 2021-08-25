58a33bf22f49a03379532d3a6357a0424f2a6393
Korey Andrus <korey86@gmail.com>
2021-08-24 22:06:41 -0500

    build client ui and connect backend

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://kandrus:abcd1234@cluster0.3gpu9.mongodb.net/memories-app?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
